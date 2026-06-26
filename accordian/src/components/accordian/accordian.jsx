2389ada7c2f794c5fe0b56ecdd041dde12ad7360
mhamzanadeem <nadeemmhamza@gmail.com>
2026-06-27 01:03:18 +0500

    implement  React  hook for Accordian

--- accordian/src/components/accordian/accordian.jsx
+++ accordian/src/components/accordian/accordian.jsx
@@ -1,9 +1,59 @@
+import data from '../../../data/data.js'
+import { useState } from 'react'
+
+
 export default function Accordian() {
+
+    const [selected, setSelected] = useState(null);
+    const [enablemultipleselection, setenablemultipleselection] = useState(false)
+    const [multiple, setmultiple] = useState([])
+
+
+    function singleSelectionHandler(id) {
+        setSelected(selected === id ? null : id);
+    }
+
+    function multipleselection(id) {
+        let temp = [...multiple]
+
+        let index = temp.indexOf(id)
+
+        if (index === -1) {
+            temp.push(id)
+        } else {
+            temp.splice(index, 1)
+        }
+
+        setmultiple(temp)
+
+    }
     return (
-        <div >
+        <>
             <div>
-                {data && data.length > 0 ? data : <div>No data found</div>}
+                <button onClick={() => setenablemultipleselection(!enablemultipleselection)}>Enable Multi Selection</button>
+                {data && data.length > 0 ? (
+                    data.map((item) => (
+                        <div>
+                            <div onClick={enablemultipleselection ? () => multipleselection(item.id) : () => singleSelectionHandler(item.id)}>
+                                <div>
+                                    <h3>{item.question}</h3>
+                                </div>
+
+                            </div>
+                            {
+                                enablemultipleselection ?
+                                    multiple.indexOf(item.id) !== -1 && (
+                                        <div>{item.answer}</div>)
+                                    :
+                                    selected === item.id && (
+                                        <div>{item.answer}</div>)
+                            }
+                        </div>
+                    ))
+                ) : (
+                    <div>No Data Found</div>
+                )}
             </div>
-        </div>
-    )
-}
+        </>
+    );
+} 
