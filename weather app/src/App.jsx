cf020fcebb952b1ed52f24a2344d619b1510f7b9
mhamzanadeem <nadeemmhamza@gmail.com>
2026-06-27 02:46:20 +0500

    Pushing weather project directory

---
+++ weather app/src/App.jsx
@@ -0,0 +1,15 @@
+import Search from "./component/search"
+import Weather from "./component/weather"
+
+function App() {
+
+  return (
+    <>
+      <Search />
+      <Weather />
+
+    </>
+  )
+}
+
+export default App
