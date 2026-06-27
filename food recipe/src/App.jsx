a54e31edbbd42631c12145afd58024f4943c9327
mhamzanadeem <nadeemmhamza@gmail.com>
2026-06-27 17:16:03 +0500

    implemented routing of food recipe app

--- food recipe/src/App.jsx
+++ food recipe/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Favorites from "./pages/favorites";
+import Details from "./pages/details";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/recipe-item/:id" element={<Details />} />
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
